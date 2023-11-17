dbb33850994862e99414c3cc64990f9555562d41
Abdulrahem Mahgoub <abdulrahimmahgoub@gmail.com>
2023-11-17 15:41:02 +0200

    Recursive shuffle and minor fixes

--- working.py
+++ working.py
@@ -1,4 +1,137 @@
-from random import shuffle
+from random import choice
+
+
+def shuffle(ls, left=None, right=0):  # fisher-yates shuffle
+    if not left:
+        ls = ls[::2] + ls[1::2]
+        left = len(ls[1::2])
+
+    if right > left:  # base case, all elements have been shuffled
+        return ls
+
+    if right < left:  # recursive step 1: if there are still elements in org list ( sorting lhs )
+        choice_left = choice(range(right, left))  # pick a random number from start to end index
+        ls[right], ls[choice_left] = ls[choice_left], ls[right]  # swap out numbers
+
+    if left + right < len(ls): # recursive step 2: if there are still elements in the right ( sorting rhs )
+        choice_right = choice(range(right+left, len(ls)))  # pick random number between mid and end ( rhs )
+        ls[-1 - right], ls[choice_right] = ls[choice_right], ls[-1 - right]  # swap
+
+    return shuffle(ls, left, right+1)
+
+
+class Table:
+    def __init__(self, player):
+        self.dealer = Dealer()
+        self.player = Player(player)
+        self.deck = Deck()
+
+        self.setup_game()
+
+    def new_game(self):
+        self.dealer.hand = self.player.hand = []
+        self.deck = Deck()
+        self.setup_game()
+
+    def setup_game(self):
+        self.deck.shuffle()  # shuffle cards before starting game
+        self.player.place_bet()
+        self.deal_card(self.player)  # deal card to player, then dealer, then player.
+        self.deal_card(self.dealer)
+        self.deal_card(self.player)
+        self.calculate_score(self.player)
+        self.calculate_score(self.dealer)
+
+        self.main()  # main game
+
+    def main(self):
+        while True:
+            print(self)
+            player_move = self.player.hit_or_stick()
+            if player_move:
+                self.deal_card(self.player)
+                self.calculate_score(self.player)
+            else:
+                self.dealer_hit()
+
+    def dealer_hit(self):
+        while True:
+            if self.dealer.score < 17:
+                self.deal_card(self.dealer)
+                self.calculate_score(self.dealer)
+                print(self)  # prints __str__
+            elif self.dealer.score >= 17:
+                self.check_final_score()
+
+    def __str__(self):
+        dealer_hand = [card for card, v in self.dealer.hand]
+        p_hand = [card for card, v in self.player.hand]
+
+        print(f"Dealer hand: {dealer_hand}")
+        print(f"Dealer score: {self.dealer.score}\n")
+
+        print(f"Your hand: {p_hand}")
+        print(f"Your score: {self.player.score}\n")
+
+        print(f"Your current bet: {self.player.bet}")
+        print(f"Your current funds: {self.player.funds}")
+        print('-' * 40)
+
+        return ''
+
+    def calculate_score(self, p):
+        val = sum(card[1] for card in p.hand)  # eg A22
+        if val <= 11 and any(card[0] == "A" for card in p.hand):
+            val += 10
+        p.score = val
+        self.check_win(p.score, p)
+
+    def deal_card(self, p):
+        deck = self.deck
+        p.hand.append(deck.stack.pop())
+
+    def check_win(self, score, player):
+        if score > 21:
+            print(self)
+            print(f"{player.name} busts")
+            if player.name == "Dealer":
+                self.player.payout()
+            self.end_game()
+        elif score == 21:
+            print(self)
+            print(f"{player.name} blackjack")
+            try:
+                player.payout()
+            except:
+                pass
+            self.end_game()
+        else:
+            return
+
+    def check_final_score(self):
+        dealer_score = self.dealer.score
+        player_score = self.player.score
+
+        if dealer_score > player_score:
+            print("Dealer wins!")
+            self.end_game()
+        else:
+            print(f"You win!")
+            self.player.payout()
+            self.end_game()
+
+    def end_game(self):
+        funds = self.player.funds
+        if funds:
+            again = input("Do you want to play again (Y/N): ")
+            if again.lower().startswith('y'):
+                self.new_game()
+            elif again.lower().startswith('n'):
+                exit(1)
+        else:
+            print("You're all out of money! Get it up and come back :(")
+            exit(0)
+
 
 class Deck:
     def __init__(self):
@@ -8,7 +141,7 @@ class Deck:
         self.shuffle()
 
     def shuffle(self):
-        shuffle(self.stack)
+        self.stack = shuffle(self.stack)
 
 
 class Dealer(object):
@@ -16,12 +149,6 @@ class Dealer(object):
         self.name = name
         self.hand = []
         self.score = 0
-
-    def hit_or_stick(self):
-        if self.score < 17:
-            return True
-        else:
-            return False
 
 
 class Player(Dealer):
@@ -32,22 +159,27 @@ class Player(Dealer):
 
     def place_bet(self):
         print(f"Your current bet is {self.bet}")
-        choice = input("Would you like to increase it? (Y/N): ").lower()
         while True:
-            if choice.startswith('y'):
-                bet = input("Enter new bet: ")
+            choice = input("Would you like to change your bet (Y/N): ")
+            if choice.startswith('n'):
+                self.funds -= self.bet
+                return
+            elif choice.startswith('y'):
+                bet = input("Enter your new bet: ")
                 try:
                     bet = float(bet)
                     self.bet = bet
+                    self.funds -= self.bet
                     return
-                except TypeError:
+                except ValueError:
                     print("Please enter valid number or (N) to exit: ")
                     continue
-            elif choice.startswith('n'):
-                return
             else:
-                print("Please enter valid input (Y/N): ")
+                print("Invalid input, (Y/N): ")
                 continue
+
+    def payout(self):
+        self.funds += self.bet * 2
 
     @staticmethod
     def hit_or_stick():
@@ -60,3 +192,9 @@ class Player(Dealer):
             else:
                 print("Enter valid choice (Y/N): ")
                 continue
+
+
+name = input("Hello, whats your name: ")
+table = Table(name)
+table.setup_game()
+table.main()
